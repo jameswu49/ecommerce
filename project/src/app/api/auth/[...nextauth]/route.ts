9e4ff5c21141200a8bb1d93b3723a48170f4d52b
Jimmy Wu <jameswu49@gmail.com>
2023-11-02 00:18:15 +0000

    added req

--- project/src/app/api/auth/[...nextauth]/route.ts
+++ project/src/app/api/auth/[...nextauth]/route.ts
@@ -4,6 +4,11 @@ import CredentialsProvider from "next-auth/providers/credentials";
 
 const prisma = new PrismaClient();
 
+type Credentials = {
+    username: string,
+    password: string | null
+}
+
 const authOptions: NextAuthOptions = {
     providers: [
         CredentialsProvider({
@@ -12,7 +17,7 @@ const authOptions: NextAuthOptions = {
                 username: { label: 'Username', type: 'text', placeholder: 'Username' },
                 password: { label: 'Password', type: 'password' },
             },
-            async authorize(credentials: any) {
+            async authorize(credentials: Credentials, req: any) {
                 try {
                     const existingUser = await prisma.user.findUnique({
                         where: { username: credentials.username, password: credentials.password },
