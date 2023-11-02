4a01efec4159df81bd372e12cec4be8effee559a
Jimmy Wu <jameswu49@gmail.com>
2023-11-02 19:54:00 +0000

    changed to string

--- project/src/app/api/auth/[...nextauth]/route.ts
+++ project/src/app/api/auth/[...nextauth]/route.ts
@@ -5,7 +5,7 @@ import CredentialsProvider from "next-auth/providers/credentials";
 const prisma = new PrismaClient();
 
 type User = {
-    id: number,
+    id: string,
     username: string,
     password: string
 }
