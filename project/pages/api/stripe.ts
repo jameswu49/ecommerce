973eb5ffe84464cf524d32e10e0a0f39b4cf8a7a
Jimmy Wu <jameswu49@gmail.com>
2023-11-07 23:37:37 +0000

    changed to string

--- project/pages/api/stripe.ts
+++ project/pages/api/stripe.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
 export default async function handler(
     req: NextApiRequest,
