ed8b3723f1367c0ccc72fd0223159b5cf580bd4d
Jimmy Wu <jameswu49@gmail.com>
2023-11-10 04:20:12 +0000

    changed navbar gap from 3to 4

--- project/src/app/components/navbar.tsx
+++ project/src/app/components/navbar.tsx
@@ -71,7 +71,7 @@ export default function Navbar() {
             <div className='absolute right-[5%] top-[30%] text-2xl md:text-3xl lg:hidden' onClick={() => handleMenuToggle()}>
                 {!isOpen ? <FiMenu /> : <AiOutlineClose />}
             </div>
-            <div className='hidden lg:flex lg:gap-x-3 lg:mr-5'>
+            <div className='hidden lg:flex lg:gap-x-4 lg:mr-5'>
                 {navbarLinks.map((links, index) => (
                     <div key={index} className='flex flex-col'>
                         <Link href={links.href} onClick={() => handleActiveLink()}>{links.name}</Link>
